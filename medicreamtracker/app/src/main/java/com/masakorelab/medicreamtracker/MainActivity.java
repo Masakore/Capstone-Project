cf32fccccbf890f1d4e9c35f7daa54a900bc8d65
Masayuki Osuka <masakore8@gmail.com>
2016-07-17 12:23:37 +0800

    Added an menu item to call register activity

--- medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/MainActivity.java
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/MainActivity.java
@@ -1,5 +1,6 @@
 package com.masakorelab.medicreamtracker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -43,8 +44,10 @@ public class MainActivity extends AppCompatActivity {
     // as you specify a parent activity in AndroidManifest.xml.
     int id = item.getItemId();
 
-    //noinspection SimplifiableIfStatement
-    if (id == R.id.action_settings) {
+    //How to call another activity from menu
+    //ref: https://developer.android.com/training/basics/firstapp/starting-activity.html
+    if (id == R.id.register_activity) {
+      startActivity(new Intent(this, RegisterActivity.class));
       return true;
     }
 
