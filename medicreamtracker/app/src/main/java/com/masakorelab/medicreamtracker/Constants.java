7b28cb95c104668986f993a890f52ab988e60fac
Masayuki Osuka <masakore8@gmail.com>
2016-07-29 10:10:45 +0800

    Add a class for constant

---
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/Constants.java
@@ -0,0 +1,7 @@
+package com.masakorelab.medicreamtracker;
+
+public class Constants {
+  public static final String COL_NAME = "CREAMENAME";
+  public static final String COL_PARTS = "BODYPARTS";
+  public static final String COL_DATE = "APPLYDATE";
+}
