fe196e6880d6621c04792d6366e980933f79ec6c
Masayuki Osuka <masakore8@gmail.com>
2016-07-30 10:48:38 +0800

    Delete unused Constants class

--- medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/Constants.java
+++
@@ -1,7 +0,0 @@
-package com.masakorelab.medicreamtracker;
-
-public class Constants {
-  public static final String COL_NAME = "CREAMENAME";
-  public static final String COL_PARTS = "BODYPARTS";
-  public static final String COL_DATE = "APPLYDATE";
-}
