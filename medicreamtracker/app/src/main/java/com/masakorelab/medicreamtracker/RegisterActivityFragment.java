5d4602c515a5c68b76356e6189552f69131c69cc
Masayuki Osuka <masakore8@gmail.com>
2016-08-08 21:07:34 +0800

    Modified register activity UI/UX

--- medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/RegisterActivityFragment.java
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/RegisterActivityFragment.java
@@ -1,15 +1,33 @@
 package com.masakorelab.medicreamtracker;
 
+import android.database.Cursor;
+import android.net.Uri;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
 
-/**
- * A placeholder fragment containing a simple view.
- */
-public class RegisterActivityFragment extends Fragment {
+import com.masakorelab.medicreamtracker.data.Contract;
+
+public class RegisterActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+  private static final int FORECAST_LOADER = 0;
+  private RegisterAdapter mRegisterAdapter;
+
+  private EditText inputName, inputDescription;
+  private TextInputLayout inputLayoutName, inputLayoutDescription;
+  private Button btnRegister;
 
   public RegisterActivityFragment() {
   }
@@ -17,6 +35,101 @@ public class RegisterActivityFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_register, container, false);
+
+    mRegisterAdapter = new RegisterAdapter(getActivity(), null, 0);
+
+    View root = inflater.inflate(R.layout.fragment_register, container, false);
+
+    ListView listView = (ListView) root.findViewById(R.id.listView_register);
+    listView.setAdapter(mRegisterAdapter);
+
+    //ref: text validation: http://www.androidhive.info/2015/09/android-material-design-floating-labels-for-edittext/
+    inputLayoutName = (TextInputLayout) root.findViewById(R.id.input_layout_name);
+    inputLayoutDescription = (TextInputLayout) root.findViewById(R.id.input_layout_description);
+    inputName = (EditText) root.findViewById(R.id.register_cream_name);
+    inputDescription = (EditText) root.findViewById(R.id.register_desctiption);
+    btnRegister = (Button) root.findViewById(R.id.btn_register);
+    inputName.addTextChangedListener(new MyTextWatcher(inputName));
+
+    btnRegister.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        submitForm();
+      }
+    });
+
+    return root;
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    getLoaderManager().initLoader(FORECAST_LOADER, null, this);
+    super.onActivityCreated(savedInstanceState);
+  }
+
+  @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    Uri mediCreamUri = Contract.MediCreamEntry.CONTENT_URI;
+    return new CursorLoader(getActivity(), mediCreamUri, null, null, null, null);
+  }
+
+  @Override
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+    mRegisterAdapter.swapCursor(data);
+  }
+
+  @Override
+  public void onLoaderReset(Loader<Cursor> loader) {
+    mRegisterAdapter.swapCursor(null);
+  }
+
+  private void submitForm() {
+    if(!validateName()) {
+      return;
+    }
+
+    Toast.makeText(getActivity(), "Added", Toast.LENGTH_SHORT).show();
+  }
+
+  private boolean validateName() {
+    if (inputName.getText().toString().trim().isEmpty()) {
+      inputLayoutName.setError(getString(R.string.register_validation_empty));
+      requestFocus(inputName);
+      return false;
+    }
+    return true;
+  }
+
+  private void requestFocus(View view) {
+    if (view.requestFocus()) {
+      getActivity().getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+    }
+  }
+
+  private class MyTextWatcher implements TextWatcher {
+    private View view;
+
+    private MyTextWatcher(View view) {
+      this.view = view;
+    }
+
+    @Override
+    public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+    }
+
+    @Override
+    public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+    }
+
+    @Override
+    public void afterTextChanged(Editable s) {
+      switch (view.getId()) {
+        case R.id.register_cream_name:
+          validateName();
+          break;
+      }
+    }
   }
 }
---
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/RegisterAdapter.java
@@ -0,0 +1,39 @@
+package com.masakorelab.medicreamtracker;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.support.v4.widget.CursorAdapter;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.masakorelab.medicreamtracker.data.Contract;
+
+public class RegisterAdapter extends CursorAdapter {
+
+  public RegisterAdapter(Context context, Cursor c, int flags) {
+    super(context, c, flags);
+  }
+
+  @Override
+  public View newView(Context context, Cursor cursor, ViewGroup parent) {
+    View view = LayoutInflater.from(context).inflate(R.layout.listview_register_row, parent, false);
+    return view;
+  }
+
+  @Override
+  public void bindView(View view, Context context, Cursor cursor) {
+    int idx_id = cursor.getColumnIndex(Contract.MediCreamEntry._ID);
+    int idx_name = cursor.getColumnIndex(Contract.MediCreamEntry.COLUMN_NAME);
+    int idx_des = cursor.getColumnIndex(Contract.MediCreamEntry.COLUMN_DESCRIPTION);
+
+    TextView id = (TextView)view.findViewById(R.id.id_register);
+    TextView name = (TextView)view.findViewById(R.id.name_register);
+    TextView description = (TextView)view.findViewById(R.id.des_register);
+
+    id.setText(cursor.getInt(idx_id));
+    name.setText(cursor.getString(idx_name));
+    description.setText(cursor.getString(idx_des));
+  }
+}
