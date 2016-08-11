5d8a938c583b063e0fc8da2ac4b2151605f1e944
Masayuki Osuka <masakore8@gmail.com>
2016-08-11 19:42:26 +0800

    Added material spinner to main page

--- medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/MainActivityFragment.java
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/MainActivityFragment.java
@@ -1,18 +1,30 @@
 package com.masakorelab.medicreamtracker;
 
+import android.database.Cursor;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 import android.support.v4.widget.CursorAdapter;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 
+import com.masakorelab.medicreamtracker.data.Contract;
+
+import fr.ganfra.materialspinner.MaterialSpinner;
+
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment {
+public class MainActivityFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+  private static final int MAIN_LOADER = 3;
+  SimpleCursorAdapter mMainAdapter;
+
+  private MaterialSpinner mNameSpinner;
 
   public MainActivityFragment() {
   }
@@ -20,17 +32,46 @@ public class MainActivityFragment extends Fragment {
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
+
     View root = inflater.inflate(R.layout.fragment_main, container, false);
 
-    //ref: https://developer.android.com/guide/topics/ui/controls/spinner.html
-//    Spinner spinner = (Spinner) root.findViewById(R.id.registered_item_spinner);
-    // Create an ArrayAdapter using the string array and a default spinner layout
-//    CursorAdapter adapter =
-    // Specify the layout to use when the list of choices appears
-//    adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-    // Apply the adapter to the spinner
-//    spinner.setAdapter(adapter);
+    mNameSpinner = (MaterialSpinner) root.findViewById(R.id.item_spinner);
+    setSpinner();
 
     return root;
+  }
+
+  private void setSpinner() {
+    String[] columns;
+    int[] to;
+
+    Cursor mediCreamCursor = getActivity().getContentResolver().query(Contract.MediCreamEntry.CONTENT_URI, null, null, null, null);
+    columns = new String[] { Contract.MediCreamEntry.COLUMN_NAME };
+    to = new int[] { android.R.id.text1 };
+    mMainAdapter = new SimpleCursorAdapter(getActivity(), android.R.layout.simple_spinner_dropdown_item, mediCreamCursor, columns, to);
+    mMainAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+    mNameSpinner.setAdapter(mMainAdapter);
+  }
+
+  @Override
+  public void onActivityCreated(Bundle savedInstanceState) {
+    getLoaderManager().initLoader(MAIN_LOADER, null, this);
+    super.onActivityCreated(savedInstanceState);
+  }
+
+  @Override
+  public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+    return null;
+  }
+
+  @Override
+  public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+
+  }
+
+  @Override
+  public void onLoaderReset(Loader<Cursor> loader) {
+
   }
 }
