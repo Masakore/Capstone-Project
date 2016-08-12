c54ee5be53439d6906f68dae9ef522b9b5a375e4
Masayuki Osuka <masakore8@gmail.com>
2016-08-13 07:07:52 +0800

    Delete unused reference

--- medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/RecordActivity.java
+++ medicreamtracker/app/src/main/java/com/masakorelab/medicreamtracker/RecordActivity.java
@@ -13,7 +13,6 @@ import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
